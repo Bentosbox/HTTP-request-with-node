f3ce96c69c47fed2722cd4da3336c76aa03e6523
Ben Li <ben_li5@yahoo.ca>
2017-03-13 19:18:13 +0000

    Require Modular step 5

---
+++ http-functions.js
@@ -0,0 +1,31 @@
+// var https = require('https');
+module.exports = function getHTML (options, callback) {
+    /* Your code here */
+  var https = require('https');
+  var str = '';
+
+  var requestOptions = {
+    host: options.host,
+    path: options.path
+  };
+
+  /* Add your code here */
+  https.get(requestOptions, function (response) {
+
+  // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+    var response = response.on('data', function (data) {
+      str += data;
+      printHTML(str);
+      // console.log(str);
+    });
+  });
+
+  function printHTML (html) {
+    console.log(html);
+  }
+};
+
+// getHTML(requestOptions);
